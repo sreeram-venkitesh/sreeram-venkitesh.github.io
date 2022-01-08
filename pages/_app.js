43b76d9472ce568a68d2afbdef39e3c7cf2e3414
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-01-08 12:07:36 +0530

    Removed unwanted GA testing config

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 
-// import { useEffect } from "react"
 import { ThemeProvider } from 'next-themes'
-// import { useRouter } from 'next/router';
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
@@ -15,21 +13,6 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
-  // const router = useRouter();
-
-  // const handleRouteChange = (url) => {
-  //   window.gtag('config', process.env.GOOGLE_ANALYTICS_ID, {
-  //     page_path: url,
-  //   });
-  // };
-  
-  // useEffect(() => {
-  //   router.events.on('routeChangeComplete', handleRouteChange);
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange);
-  //   };
-  // }, [router.events]);
-
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
