7501d67d3c9986ee0d0eb14aa6de0b930017baec
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-04-04 22:09:09 +0530

    Open source challenge day 1

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,9 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'Posts' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   // { href: '/talks', title: 'Talks' },
+  // { href: '/blog/oss-logs', title: 'Log'},
   { href: '/about', title: 'About' },
 ]
 
