f472228e4e80d0616ec1045ef21f377d3c98f9d6
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2023-11-04 19:35:52 +0530

    Removed ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -4,14 +4,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com *.google-analytics.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
-  frame-src giscus.app
 `
 
 const securityHeaders = [
